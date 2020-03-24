76d812bc811d854511cc7ecb3195c6590c14aeab
macarro <migmackle@alum.us.es>
2020-03-24 11:39:00 +0100

    Format services

--- src/main/java/org/group2/petclinic/service/UserService.java
+++ src/main/java/org/group2/petclinic/service/UserService.java
@@ -1,20 +1,5 @@
-/*
- * Copyright 2002-2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.group2.petclinic.service;
 
+package org.group2.petclinic.service;
 
 import org.group2.petclinic.model.User;
 import org.group2.petclinic.repository.UserRepository;
@@ -23,21 +8,20 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-/**
- * Mostly used as a facade for all Petclinic controllers Also a placeholder
- * for @Transactional and @Cacheable annotations
- *
- * @author Michael Isvy
- */
 @Service
 public class UserService {
 
 	private UserRepository userRepository;
 
+
+	// CONSTRUCTOR ------------------------------------------------------------
+
 	@Autowired
 	public UserService(UserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
+
+	// SAVE USER --------------------------------------------------------------
 
 	@Transactional
 	public void saveUser(User user) throws DataAccessException {
