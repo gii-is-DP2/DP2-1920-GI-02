0dfd0be1655af541c3d731383e0d5a11b2f5fa4d
claguecue <claudiart97@hotmail.com>
2020-04-30 22:58:32 +0200

    Add dependence of chromedriver.exe

--- src/test/java/org/group2/petclinic/UITests/admin/steps/AdminAddMedicinePositiveUITest.java
+++ src/test/java/org/group2/petclinic/UITests/admin/steps/AdminAddMedicinePositiveUITest.java
@@ -30,6 +30,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.junit.CucumberOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.extern.java.Log;
 
 @Log
